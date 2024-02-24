b45ab38fbcbf10ab8cd30997f57224b8525ad3be
Bildad Ronoh <Bildadronoh@gmail.com>
2024-02-24 23:29:21 +0300

    Mongo Db project

---
+++ app.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+const mongoose = require('mongoose');
+
+//Import Routes
+const postsRoute = require('./routes/posts');
+
+app.use('/posts',postsRoute);
+
+
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('We are on Home');
+});
+
+app.get('/posts', (req, res) => {
+    res.send('We are on Posts');
+});
+
+// Connect to DB
+
+const dbURI = process.env.DB_URI; // It's best practice to use environment variables for your URI
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to DB!'))
+  .catch(err => console.error(err));
+  
+// Start listening to the server
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`Server running locally: http://localhost:${port}`));
