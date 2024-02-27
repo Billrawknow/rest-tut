973bbd93a51c3916a98af272e85cbafa853ce791
Bildad Ronoh <Bildadronoh@gmail.com>
2024-02-27 07:40:54 +0300

    updates

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const router = express.Router();
+const Post = require('../models/Post');
 
 router.get ('/', (req, res) => {
     res.send('We are on Posts');
@@ -11,5 +12,10 @@ router.get('/specific', (req, res) => {
 });
 
 
+router.post('/',(req, res)=> {
+    const post = new Post ({
+        title : req.body.title,
+    })
+});
 
 module.exports = router;
